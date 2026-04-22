22e46ed2a4b41b41515195f01133f672318f5c27
Yunjeong So <108862582+yunso-bio@users.noreply.github.com>
2026-04-22 17:47:16 +0200

    commented

---
+++ scripts/01_fastQC.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#PBS -W group_list=dtu_00032 -A dtu_00032
+#PBS -N qc
+#PBS -e qc.err
+#PBS -o qc.log
+#PBS -M yunso@dtu.dk
+#PBS -l nodes=1:thinnode:ppn=40
+#PBS -l mem=188gb
+#PBS -l walltime=12:00:00
+
+# Load modules
+module load tools
+module load perl
+module load java/1.8.0
+module load fastqc/0.11.9
+module load multiqc/1.12
+
+# Setup working directory and paths
+path="path to working directory"
+mkdir -p ${path}/results/qc/{fastqc,multiqc}
+samples=$(cat "Path to sample list file")
+
+# Run FastQC
+for sample in ${samples}; do
+    f="${path}/data/raw/${sample}_1.fq.gz"
+    r="${path}/data/raw/${sample}_2.fq.gz"
+	fastqc -o qc/fastqc -t 40 $f $r
+done
+
+# print out the number of processed samples
+echo
+n=$(ls qc/fastqc/*1_fastqc.html | wc -l)
+echo $n
+
+# Run multiQC
+multiqc qc/fastqc multiqc-HR
+
+exit
