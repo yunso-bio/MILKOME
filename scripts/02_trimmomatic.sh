7f6fe4ca0b54d472543df9e16f3d60f197fb016e
Yunjeong So <108862582+yunso-bio@users.noreply.github.com>
2026-04-22 17:43:20 +0200

    Update and rename 01_trimmomatic.sh to 02_trimmomatic.sh

---
+++ scripts/02_trimmomatic.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#PBS -W group_list=dtu_00032 -A dtu_00032
+#PBS -N trim
+#PBS -e trim.err
+#PBS -o trim.log
+#PBS -M yunso@dtu.dk
+#PBS -l nodes=1:thinnode:ppn=40
+#PBS -l mem=188gb
+#PBS -l walltime=72:00:00
+
+# Load modules
+module load tools
+module load perl
+module load java/1.8.0
+module load fastqc/0.11.9
+module load trimmomatic/0.38
+
+# Setup working directory and paths
+path="path to working directory"
+samples=$(cat "Path to a list of samples")
+raw_path="${path}/data/raw"
+trim_path="${path}/data/trimmomatic"
+mkdir -p $trim_path
+
+# Run Trimommatic
+for sample in ${samples}; do
+    f="${raw_path}/${sample}_1.fq.gz"
+    r="${raw_path}/${sample}_2.fq.gz"
+    echo 'processing' $sample
+    java -jar /services/tools/trimmomatic/0.38/trimmomatic-0.38.jar PE -threads 40 -phred33 -trimlog logfile $f $r "${trim_path}/${sample}_trimmed_1.fq.gz" "${trim_path}/${sample}_trimmed_unpaired_1.fq.gz" "${trim_path}/${sample}_trimmed_2.fq.gz" "${trim_path}/${sample}_trimmed_unpaired_2.fq.gz" LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
+done
