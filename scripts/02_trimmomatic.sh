12085537ff0f7b9143e729531df675f24e4cbc02
Yunjeong So <108862582+yunso-bio@users.noreply.github.com>
2026-04-23 10:45:15 +0200

    Refactor paths and add trimlog output in script
    Updated paths to remove unnecessary quotes and added trimlog file output.

--- scripts/02_trimmomatic.sh
+++ scripts/02_trimmomatic.sh
@@ -3,7 +3,6 @@
 #PBS -N trim
 #PBS -e trim.err
 #PBS -o trim.log
-#PBS -M yunso@dtu.dk
 #PBS -l nodes=1:thinnode:ppn=40
 #PBS -l mem=188gb
 #PBS -l walltime=72:00:00
@@ -18,14 +17,14 @@ module load trimmomatic/0.38
 # Setup working directory and paths
 path="path to working directory"
 samples=$(cat "Path to a list of samples")
-raw_path="${path}/data/raw"
-trim_path="${path}/results/trimmomatic"
+raw_path="$path/data/raw"
+trim_path="$path/results/trimmomatic"
 mkdir -p $trim_path
 
 # Run Trimommatic
 for sample in ${samples}; do
-    f="${raw_path}/${sample}_1.fq.gz"
-    r="${raw_path}/${sample}_2.fq.gz"
+    f="$raw_path/${sample}_1.fq.gz"
+    r="$raw_path/${sample}_2.fq.gz"
     echo 'processing' $sample
-    java -jar /services/tools/trimmomatic/0.38/trimmomatic-0.38.jar PE -threads 40 -phred33 -trimlog logfile $f $r "${trim_path}/${sample}_trimmed_1.fq.gz" "${trim_path}/${sample}_trimmed_unpaired_1.fq.gz" "${trim_path}/${sample}_trimmed_2.fq.gz" "${trim_path}/${sample}_trimmed_unpaired_2.fq.gz" LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
+    java -jar /services/tools/trimmomatic/0.38/trimmomatic-0.38.jar PE -threads 40 -phred33 "$trim_path/${sample}_trimlog.txt" $f $r "$trim_path/${sample}_trimmed_1.fq.gz" "$trim_path/${sample}_trimmed_unpaired_1.fq.gz" "$trim_path/${sample}_trimmed_2.fq.gz" "$trim_path/${sample}_trimmed_unpaired_2.fq.gz" LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
 done
