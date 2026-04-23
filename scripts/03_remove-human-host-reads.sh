1329da5fcafee297abdbb8fa8f44f471157f202a
Yunjeong So <108862582+yunso-bio@users.noreply.github.com>
2026-04-23 10:55:02 +0200

    commented

--- scripts/03_remove-human-host-reads.sh
+++ scripts/03_remove-human-host-reads.sh
@@ -13,20 +13,20 @@ module load bowtie2/2.5.2
 
 # Setup working directory and paths
 path="path to working directory"
-mkdir -p ${path}/results/{host_removed,bowtie}
+mkdir -p $path/results/{host_removed,bowtie}
 samples=$(cat "Path to sample list file")
 
 # Build Bowtie2 index for the human reference genome (GRCh38)
-bowtie2-build ${path}/data/db/ncbi_dataset/GCF_000001405.40/GCF_000001405.40_GRCh38.p14_genomic.fna ${path}/data/db/GRCh38_noalt_as
+bowtie2-build $path/data/db/ncbi_dataset/GCF_000001405.40/GCF_000001405.40_GRCh38.p14_genomic.fna ${path}/data/db/GRCh38_noalt_as
 
 # Map trimmed reads to human reference (GRCh38) and extract unmapped (host-removed) reads
 for sample in $samples; do
-    f="${path}/results/trimmomatic/${sample}_trimmed_1.fq.gz"; 
-    r="${path}/results/trimmomatic/${sample}_trimmed_2.fq.gz"
+    f="$path/results/trimmomatic/${sample}_trimmed_1.fq.gz"; 
+    r="$path/results/trimmomatic/${sample}_trimmed_2.fq.gz"
     echo 
     echo 'processing' $sample
     bowtie2 -p 40 \
-            -x ${path}/data/db/GRCh38_noalt_as \
+            -x $path/data/db/GRCh38_noalt_as \
             -1 $f -2 $r \
-            --un-conc-gz "${path}/results/host_removed/${sample}" > "${path}/results/bowtie/${sample}_mapped_unmapped.sam"
+            --un-conc-gz "$path/results/host_removed/${sample}_%.fq.gz" > "$path/results/bowtie/${sample}_mapped_unmapped.sam"
 done
