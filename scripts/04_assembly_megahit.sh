a704dc4467c4e07ced6990cea8c6353ab429c37a
Yunjeong So <108862582+yunso-bio@users.noreply.github.com>
2026-04-23 21:52:38 +0200

    commented, workflow for both stool and enriched samples
    Updated script to process stool and enriched samples with MEGAHIT.

--- scripts/04_assembly_megahit.sh
+++ scripts/04_assembly_megahit.sh
@@ -14,16 +14,29 @@ module load megahit/1.2.9
 
 # Setup working directory and paths
 path="path to working directory"
-mkdir -p $path/results/megahit
-samples=$(cat "path to a list of sampels")
+mkdir -p "$path/results/megahit"
+
+stool_samples=$(cat "path to a list of stool samples")
 clean_path="$path/results/host_removed"
 
-# Run MEGAHIT to assemble contigs
-for sample in ${samples}; do
+# Run MEGAHIT for stool samples to assemble contigs
+for sample in ${stool_samples}; do
     f="$clean_path/${sample}_1.fq.gz"
     r="$clean_path/${sample}_2.fq.gz"
-    echo 'processing' $sample
-    megahit -1 $f -2 $r -o "$path/results/megahit/$sample"
+    echo "processing $sample"
+    megahit -1 "$f" -2 "$r" -o "$path/results/megahit/$sample"
+done
+
+# Setup working directory and paths
+enriched_samples=$(cat "path to a list of enriched samples")
+trim_path="$path/results/trimmomatic"
+
+# Run MEGAHIT for enriched samples to assemble contigs
+for sample in ${enriched_samples}; do
+    f="$trim_path/${sample}_trimmed_1.fq.gz"
+    r="$trim_path/${sample}_trimmed_2.fq.gz"
+    echo "processing $sample"
+    megahit -1 "$f" -2 "$r" -o "$path/results/megahit/$sample"
 done
 
 echo 'Job is done' | mail -s 'megahit is done' yunso@dtu.dk
