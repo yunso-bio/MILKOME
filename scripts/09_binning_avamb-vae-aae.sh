f775e227a5ebbacfcbe96f2b9352df9b8c1939a3
Yunjeong So <108862582+yunso-bio@users.noreply.github.com>
2026-04-23 22:33:15 +0200

    commented, Refactor Avamb binning script with new paths
    Updated paths for working directory and sample files in the Avamb binning script. Added handling for enriched samples.

---
+++ scripts/09_binning_avamb-vae-aae.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#PBS -W group_list=dtu_00032 -A dtu_00032
+#PBS -N avamb
+#PBS -e avamb.err
+#PBS -o avamb.log
+#PBS -l nodes=1:thinnode:ppn=40
+#PBS -l mem=188gb
+#PBS -l walltime=72:00:00
+
+#Load modules 
+module load tools
+module load minimap2/2.24r1122
+module load samtools/1.18
+module load vamb/4.1.3 
+
+# Setup working directory and paths
+path="path to working directory"
+mkdir -p $path/result/bins/avamb/{catalogue,bams,sorted,bins}
+stool_samples=$(cat "path to a list of stool samples")
+
+# Run Avamb for stool samples with vae-aaee model
+for N in $samples; do
+    assembly="$path/result/megahit/${N}/final.contigs.fa"
+    f="$path/result/host_removed/${sample}_1.fq.gz"
+    r="$path/result/host_removed/${sample}_2.fq.gz"
+    bam="$path/result/bins/avamb/bams/${N}.bam"
+    sorted="$path/result/bins/avamb/sorted/${N}_sorted.bam"
+    mmi="$path/result/bins/avamb/catalogue/${N}.mmi"
+
+    # Index sample
+    echo 'processing' $N 
+    minimap2 -t 40 -d $mmi $assembly
+    minimap2 -t 40 -N 5 -ax sr $mmi --split-prefix mmsplit $f $r | samtools view -F 3584 -b --threads 40 > $bam
+    echo 'processing' $N
+    samtools sort $bam -@ 40 -o $sorted 
+    
+    # Run Vamb
+    vamb --outdir $path/result/bins/avamb/bins/${N} --model vae-aae --fasta $assembly --bamfiles $sorted --minfasta 200000
+done
+
+# Run Avamb for enriched samples with vae-aaee model
+enriched_samples=$(cat "path to a list of stool samples")
+
+for N in $samples; do
+    assembly="$path/result/megahit/${N}/final.contigs.fa"
+    f="$path/result/trimmomatic/${N}_trimmed_1.fq.gz" 
+    r="$path/result/trimmomatic/${N}_trimmed_2.fq.gz"
+    bam="$path/result/bins/avamb/bams/${N}.bam"
+    sorted="$path/result/bins/avamb/sorted/${N}_sorted.bam"
+    mmi="$path/result/bins/avamb/catalogue/${N}.mmi"
+
+    # Index sample
+    echo 'processing' $N 
+    minimap2 -t 40 -d $mmi $assembly
+    minimap2 -t 40 -N 5 -ax sr $mmi --split-prefix mmsplit $f $r | samtools view -F 3584 -b --threads 40 > $bam
+    echo 'processing' $N
+    samtools sort $bam -@ 40 -o $sorted 
+    
+    # Run Vamb
+    vamb --outdir $path/result/bins/avamb/bins/${N} --model vae-aae --fasta $assembly --bamfiles $sorted --minfasta 200000
+done
