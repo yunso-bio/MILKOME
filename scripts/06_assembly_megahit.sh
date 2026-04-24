ff76f25a3e0212f89d3eb829a319858bbefa2f03
Yunjeong So <108862582+yunso-bio@users.noreply.github.com>
2026-04-24 11:30:07 +0200

    Update paths to use BASE variable for consistency

--- scripts/06_assembly_megahit.sh
+++ scripts/06_assembly_megahit.sh
@@ -13,30 +13,30 @@ module load tools
 module load megahit/1.2.9
 
 # Setup working directory and paths
-path="path to working directory"
-mkdir -p "$path/results/megahit"
+BASE="path to working directory"
+mkdir -p "$BASE/results/megahit"
 
 stool_samples=$(cat "path to a list of stool samples")
-clean_path="$path/results/host_removed"
+clean_path="$BASE/results/host_removed"
 
 # Run MEGAHIT for stool samples to assemble contigs
 for sample in ${stool_samples}; do
     f="$clean_path/${sample}_1.fq.gz"
     r="$clean_path/${sample}_2.fq.gz"
     echo "processing $sample"
-    megahit -1 "$f" -2 "$r" -o "$path/results/megahit/$sample"
+    megahit -1 "$f" -2 "$r" -o "$BASE/results/megahit/$sample"
 done
 
 # Setup working directory and paths
 enriched_samples=$(cat "path to a list of enriched samples")
-trim_path="$path/results/trimmomatic"
+trim_path="$BASE/results/trimmomatic"
 
 # Run MEGAHIT for enriched samples to assemble contigs
 for sample in ${enriched_samples}; do
     f="$trim_path/${sample}_trimmed_1.fq.gz"
     r="$trim_path/${sample}_trimmed_2.fq.gz"
     echo "processing $sample"
-    megahit -1 "$f" -2 "$r" -o "$path/results/megahit/$sample"
+    megahit -1 "$f" -2 "$r" -o "$BASE/results/megahit/$sample"
 done
 
 echo 'Job is done' | mail -s 'megahit is done' yunso@dtu.dk
