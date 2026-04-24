07341efd46db775e89aa3350bb6efb35548cf571
Yunjeong So <108862582+yunso-bio@users.noreply.github.com>
2026-04-24 14:25:34 +0200

    Add 09_07_bins_gtdb.sh script

--- scripts/09_07_bins_gtdb.sh
+++ scripts/09_07_bins_gtdb.sh
@@ -8,19 +8,18 @@
 #PBS -l mem=188gb
 #PBS -l walltime=24:00:00
 
+# Load modules
 module load tools
 module load gcc/12.2.0
 module load fastani/1.34
 module load dashing/0.4.0
 module load gtdbtk/2.3.2
 
-cd /home/projects/dtu_00032/analysis/milk-cohort/bins/new-ani98
+base_dir="path to a working directory"
 
- 
+# Run gtdbtk
 gtdbtk classify_wf --cpus 40 \
-				   --genome_dir bins \
+				   --genome_dir $base_dir/bins/ani98 \
 				   -x "fna" \
-				   --out_dir gtdb \
+				   --out_dir  $base_dir/bins/gtdb \
 				   --skip_ani_screen;
-
-mail -s 'GTDB is done' yunso@dtu.dk
