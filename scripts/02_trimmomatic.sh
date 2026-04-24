9574e5d960f03cf4b611cecfee134e480ba76438
Yunjeong So <108862582+yunso-bio@users.noreply.github.com>
2026-04-24 13:21:51 +0200

    Update Trimmomatic command

--- scripts/02_trimmomatic.sh
+++ scripts/02_trimmomatic.sh
@@ -26,5 +26,5 @@ for sample in ${samples}; do
     f="$raw_path/${sample}_1.fq.gz"
     r="$raw_path/${sample}_2.fq.gz"
     echo 'processing' $sample
-    java -jar /services/tools/trimmomatic/0.38/trimmomatic-0.38.jar PE -threads 40 -phred33 "$trim_path/${sample}_trimlog.txt" $f $r "$trim_path/${sample}_trimmed_1.fq.gz" "$trim_path/${sample}_trimmed_unpaired_1.fq.gz" "$trim_path/${sample}_trimmed_2.fq.gz" "$trim_path/${sample}_trimmed_unpaired_2.fq.gz" LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
+    java -jar /services/tools/trimmomatic/0.38/trimmomatic-0.38.jar PE -threads 40 -phred33 -trimlog "$trim_path/${sample}_trimlog.txt" $f $r "$trim_path/${sample}_trimmed_1.fq.gz" "$trim_path/${sample}_trimmed_unpaired_1.fq.gz" "$trim_path/${sample}_trimmed_2.fq.gz" "$trim_path/${sample}_trimmed_unpaired_2.fq.gz" LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:50
 done
